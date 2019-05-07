28043c91fc739532818f5a1ffefff5f0f20e8b8e
Your NameAleixer <aleixer@proax.org>
2019-05-07 13:49:38 -0500

    Tetris-primera-carga

---
+++ data.js
@@ -0,0 +1,9 @@
+export  const data = [{ "posicion": 1, "cordenadas": { "x": 75, "y": 100 }, "select": false, "jugador": 0 },
+{ "posicion": 2, "cordenadas": { "x": 145, "y": 100 }, "select": false, "jugador": 0 },
+{ "posicion": 3, "cordenadas": { "x": 215, "y": 100 }, "select": false, "jugador": 0 },
+{ "posicion": 4, "cordenadas": { "x": 75, "y": 170 }, "select": false, "jugador": 0 },
+{ "posicion": 5, "cordenadas": { "x": 145, "y": 170 }, "select": false, "jugador": 0 },
+{ "posicion": 6, "cordenadas": { "x": 215, "y": 170 }, "select": false, "jugador": 0 },
+{ "posicion": 7, "cordenadas": { "x": 75, "y": 240 }, "select": false, "jugador": 0 },
+{ "posicion": 8, "cordenadas": { "x": 145, "y": 240 }, "select": false, "jugador": 0 },
+{ "posicion": 9, "cordenadas": { "x": 215, "y": 240 }, "select": false, "jugador": 0 }];
